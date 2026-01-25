e758ec4ef44d40b4dbe46cc8bc22efd38b6ddb85
rob <somethingobscure@gmail.com>
2026-01-25 17:27:26 -0600

    implemented trait for converting any char, char array, &str and String into a Vec<char>

---
+++ src/char_to_vec.rs
@@ -0,0 +1,34 @@
+/// Types that implement CharToVec promise the ability to turn whatever data they hold into a [Vec\<char\>]
+pub trait CharToVec {
+	fn to_vec(&self) -> Vec<char>;
+}
+
+impl CharToVec for char {
+	fn to_vec(&self) -> Vec<char> {
+		vec![self.clone()]
+	}
+}
+
+impl<const N: usize> CharToVec for [char; N] {
+	fn to_vec(&self) -> Vec<char> {
+		let mut list = vec![];
+
+		for e in self {
+			list.push(e.to_owned());
+		}
+
+		list
+	}
+}
+
+impl CharToVec for String {
+	fn to_vec(&self) -> Vec<char> {
+		self.chars().collect()
+	}
+}
+
+impl CharToVec for &str {
+	fn to_vec(&self) -> Vec<char> {
+		self.chars().collect()
+	}
+}
